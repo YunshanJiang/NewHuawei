8f26058cb11e59765411708e90d4d7a537080185
YunshanJiang <yunshantree@gmail.com>
2025-06-04 02:10:58 +0800

    debug spawn text

--- HuaweiWall/Source/HuaweiWall/APIManager.cpp
+++ HuaweiWall/Source/HuaweiWall/APIManager.cpp
@@ -101,8 +101,14 @@ void AAPIManager::LoadJsonData(FString& JsonContent)
 
     if (FJsonSerializer::Deserialize(JsonReader, JsonArray))
     {
+        int tempCounter = 0;
         for (const TSharedPtr<FJsonValue>& Value : JsonArray)
         {
+            tempCounter++;
+            if (PathArray.Num() >= tempCounter)
+            {
+				continue; // Skip if PathArray already has enough entries
+            }
             if (Value.IsValid() && Value->Type == EJson::Object)
             {
                 TSharedPtr<FJsonObject> JsonObject = Value->AsObject();
